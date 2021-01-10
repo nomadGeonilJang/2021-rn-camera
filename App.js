ce26bea7afac3053659fdc0f8ee04ce56f289191
nomadGeonilJang <wkdrjsdlf2@gmail.com>
2021-01-10 22:21:52 +0900

    Add Camera

--- App.js
+++ App.js
@@ -1,21 +1,80 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {  useLayoutEffect,useState, useRef } from 'react';
+import {  View , TouchableOpacity, Button, ActivityIndicator} from 'react-native';
+import { Camera } from 'expo-camera';
+import styled from "styled-components/native"
 
 export default function App() {
+
+  const cameraRef = useRef()
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+
+
+  const handleSnap = async () => {
+    const snap = await cameraRef.current.takePictureAsync();
+    console.log(snap)
+  }
+
+
+  useLayoutEffect(()=>{
+    (async()=>{
+      const { status} = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  },[])
+
+  if (hasPermission === null) {
+    return <ActivityIndicator/>
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <CenterView >
+      {hasPermission !== true ? (
+        <Text> No Permissions</Text>
+      ) : hasPermission !== false ? 
+      (
+        (
+          <>
+          <Camera ref={cameraRef} type={type} >
+            <View >
+              <TouchableOpacity
+                onPress={() => {
+                  console.log(Camera.Constants.Type.front)
+                  setType(
+                    type === Camera.Constants.Type.back
+                      ? Camera.Constants.Type.front
+                      : Camera.Constants.Type.back
+                  );
+                }}>
+                <Text> Flip </Text>
+              </TouchableOpacity>
+            </View>
+          </Camera>
+              <Button title="snap"/>
+              <StatusBar style="auto" />
+          </>
+        ) 
+      )
+      : (<ActivityIndicator/>)}
+    
+       </CenterView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const CenterView = styled.View`
+  flex:1;
+  align-items:center;
+  justify-content:center;
+  background-color:cornflowerblue;
+`
+
+const Text = styled.Text`
+  color:purple;
+
+`
+
